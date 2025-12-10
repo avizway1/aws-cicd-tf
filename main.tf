e5dd6c818f8f63b5bb6b5ee886cb8d4c85955ff0
Avinash <avizway@avizacademy.com>
2025-12-10 10:05:23 +0530

    added manual step

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "web" {
   ami           = "ami-00ca570c1b6d79f36"
   instance_type = "t3.micro"
   tags = {
-    Name = "tf-cicd-update"
+    Name = "tf-manual-verify"
   }
 }
