571a83ec5053c1365186232eb16038b2092a09b2
Avinash <avizway@avizacademy.com>
2025-12-10 10:00:33 +0530

    adjusted the instance config

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-00ca570c1b6d79f36"
-  instance_type = "t3.small"
+  instance_type = "t3.micro"
   tags = {
-    Name = "tf-cicd-test"
+    Name = "tf-cicd-update"
   }
 }
