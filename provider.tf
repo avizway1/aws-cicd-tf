cbfb7474d289c8209b9f96ecf6fdc3d46d4d2c4b
Avinash <avizway@avizacademy.com>
2025-12-10 09:49:02 +0530

    first commit

---
+++ provider.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+terraform {
+  required_version = ">= 1.0"
+
+    backend "s3" {
+      bucket = "aviz-terraform-state"
+      key = "cicd/ec2/terraform.tfstate"
+      region = "ap-south-1"
+    }
+
+}
