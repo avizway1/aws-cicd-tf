e124eb560afc032b18256e7efa616dbfd07bfc39
Avinash Reddy Thipparthi <57447641+avizway1@users.noreply.github.com>
2026-03-06 22:20:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = "ami-00ca570c1b6d79f36"
+  ami           = "ami-051a31ab2f4d498f5"
   instance_type = "t3.micro"
   tags = {
     Name = "tf-manual-verify"
