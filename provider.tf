e1f570f756a54bcef19119dfc669fb8dc011fdc5
Avinash Reddy Thipparthi <57447641+avizway1@users.noreply.github.com>
2026-03-06 22:19:25 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,10 +3,9 @@ provider "aws" {
 }
 
 terraform {
-  required_version = ">= 1.0"
 
   backend "s3" {
-    bucket = "aviz-terraform-state"
+    bucket = "terraform-state-store-aviz"
     key    = "cicd/ec2/terraform.tfstate"
     region = "ap-south-1"
   }
